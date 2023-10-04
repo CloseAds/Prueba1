23652e026284a72b7b8d4e38a75123c76828ce0f
CloseAds <c7336188@gmail.com>
2023-10-04 10:03:55 +0200

    prueba1

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a, b, c;
+        System.out.println(a * b +c * ((3 -a)/(2*b)));
+        }
+    }
+
---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println(" Introduce grados centígrados ");
+        int gradosCentígrados;
+
+
+        }
+    }
